50f227fde46e13ca37b7e21747bf94cc1228b5b9
shubhankari-1 <117729333+shubhankari-1@users.noreply.github.com>
2025-12-11 11:18:54 +0530

    added to git

--- SeleniumProject/src/test/java/pac2/OrgTest.java
+++ SeleniumProject/src/test/java/pac2/OrgTest.java
@@ -7,6 +7,15 @@ public class OrgTest {
 	@Test
 	public void createOrgTest() {
 		System.out.println("execute createOrgTest");
+		
+		String URL=System.getProperty("url");
+		String BROWSER=System.getProperty("browser");
+		String USERNAME=System.getProperty("username");
+		String PASSWORD=System.getProperty("password");
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
 	}
 	@Test
 	public void modifyOrgTest() {
